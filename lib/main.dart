df3acd7fa4053d4210a093c0e263252636b62b09
Umut Küniç <ukunic243@gmail.com>
2026-02-11 19:44:46 +0300

    chore: align Android NDK version for Firebase plugins

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'core/theme/app_theme.dart';
 import 'shared/widgets/app_scaffold.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const PateDateApp());
 }
 
